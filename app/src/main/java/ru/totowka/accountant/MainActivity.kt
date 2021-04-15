d9a616f48cf42c4e1620855193f24ad6af8d055e
Tigran <tigran.kocharyan@outlook.com>
2021-04-15 22:21:11 +0300

    Add firestore connection

--- app/src/main/java/ru/totowka/accountant/MainActivity.kt
+++ app/src/main/java/ru/totowka/accountant/MainActivity.kt
@@ -1,11 +1,16 @@
 package ru.totowka.accountant
 
 import android.os.Bundle
+import android.util.Log
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+import java.lang.StringBuilder
 
 class MainActivity :  AppCompatActivity() {
 
+    val db = Firebase.firestore
     lateinit var content: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,8 +18,29 @@ class MainActivity :  AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         content = findViewById(R.id.content)
+        val updateResult = updateContent()
+
+        content.text = updateResult
     }
 
+    fun updateContent() : String {
+        val content = db.collection("purchase_info")
+        val stringBuilder: StringBuilder = StringBuilder();
+        content.get()
+            .addOnSuccessListener { result ->
+                for(document in result){
+                    Log.d(TAG,"${document.id}=>${document.data}")
+                    stringBuilder.append("${document.id} -> ${document.data}\n")
+                }
+            }
+            .addOnFailureListener { exception ->
+                Log.d(TAG, "get failed with ", exception)
+            }
 
+        return stringBuilder.toString()
+    }
 
+    companion object {
+        const val TAG = "MainActivity"
+    }
 }
