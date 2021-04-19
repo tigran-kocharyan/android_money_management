eff51bdd3a9e02fcfdbcd2beded995426d937aca
Tigran <tigran.kocharyan@outlook.com>
2021-04-19 12:51:40 +0300

    Fix firestore connection

--- app/src/main/java/ru/totowka/accountant/AuthActivity.kt
+++ app/src/main/java/ru/totowka/accountant/AuthActivity.kt
@@ -2,56 +2,64 @@ package ru.totowka.accountant
 
 import android.app.Activity
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.firebase.ui.auth.AuthUI
-import com.firebase.ui.auth.IdpResponse
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 
 class AuthActivity : AppCompatActivity() {
 
     private lateinit var mSignin: Button
+    private lateinit var mTitle: TextView
+    private val mAuth: FirebaseAuth = FirebaseAuth.getInstance()
+    private val db = Firebase.firestore
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_auth)
-
+        mTitle = findViewById<TextView>(R.id.title)
         mSignin = findViewById(R.id.sign_in)
         mSignin.setOnClickListener(View.OnClickListener {
-            val providers = arrayListOf(
-                AuthUI.IdpConfig.GoogleBuilder().build()
-            )
-
-            startActivityForResult(
-                AuthUI.getInstance()
-                    .createSignInIntentBuilder()
-                    .setAvailableProviders(providers)
-                    .build(),
-                RC_SIGN_IN
-            )
+            when (FirebaseAuth.getInstance().currentUser) {
+                null -> {
+                    val providers = arrayListOf(
+                        AuthUI.IdpConfig.GoogleBuilder().build()
+                    )
+                    startActivityForResult(
+                        AuthUI.getInstance()
+                            .createSignInIntentBuilder()
+                            .setAvailableProviders(providers)
+                            .build(),
+                        RC_SIGN_IN
+                    )
+                }
+                else -> {
+                    startActivity(Intent(this, MainActivity::class.java))
+                }
+            }
         })
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
-
         if (requestCode == RC_SIGN_IN) {
-            val response = IdpResponse.fromResultIntent(data)
             if (resultCode == Activity.RESULT_OK) {
-                val user = FirebaseAuth.getInstance().currentUser
-                val intent: Intent = Intent(
-                    this,
-                    MainActivity::class.java
-                ).apply {
-                    putExtra("user_name", user)
-                }
-                startActivity(intent)
+                val user = hashMapOf<String, Any>(
+                    "uid" to mAuth.currentUser!!.uid
+                )
+                db.collection("users")
+                    .document(mAuth.currentUser!!.uid)
+                    .set(user)
+                startActivity(Intent(this, MainActivity::class.java))
             } else {
-                val toast = Toast.makeText(this, ERROR_SIGN_IN, Toast.LENGTH_SHORT)
-                toast.show()
+                Toast.makeText(this, ERROR_SIGN_IN, Toast.LENGTH_SHORT).show()
             }
         }
     }
--- app/src/main/java/ru/totowka/accountant/MainActivity.kt
+++ app/src/main/java/ru/totowka/accountant/MainActivity.kt
@@ -2,45 +2,84 @@ package ru.totowka.accountant
 
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.Timestamp
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 import java.lang.StringBuilder
+import java.util.*
 
-class MainActivity :  AppCompatActivity() {
+class MainActivity :  AppCompatActivity(), View.OnClickListener {
 
     val db = Firebase.firestore
-    lateinit var content: TextView
+    lateinit var mContent: TextView
+    lateinit var mUpdateContent: Button
+    lateinit var mGetContent: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        content = findViewById(R.id.content)
-        val updateResult = updateContent()
+        mContent = findViewById(R.id.content)
+        mUpdateContent = findViewById(R.id.update_content)
+        mGetContent = findViewById(R.id.get_content)
 
-        content.text = updateResult
+        mUpdateContent.setOnClickListener(this)
+        mGetContent.setOnClickListener(this)
     }
 
-    fun updateContent() : String {
+    fun getContent() {
+        Log.d(TAG, "Clicked getContent()")
         val content = db.collection("purchase_info")
         val stringBuilder: StringBuilder = StringBuilder();
         content.get()
             .addOnSuccessListener { result ->
-                for(document in result){
-                    Log.d(TAG,"${document.id}=>${document.data}")
-                    stringBuilder.append("${document.id} -> ${document.data}\n")
+                run {
+                    for (document in result) {
+                        Log.d(TAG, "${document.id}=>${document.data}")
+                        stringBuilder.appendln("${document.id} -> ${document.data}\n")
+                    }
+                    mContent.text = stringBuilder.toString()
                 }
             }
             .addOnFailureListener { exception ->
                 Log.d(TAG, "get failed with ", exception)
             }
 
-        return stringBuilder.toString()
+        Log.d(TAG, "The data from StringBuilder => $stringBuilder")
     }
+
+    fun updateContent() {
+        Log.d(TAG, "Clicked updateContent()")
+        val docData = hashMapOf(
+            "date" to Timestamp(Date()),
+            "items" to hashMapOf(
+                "amount" to 2,
+                "price" to 100,
+                "title" to "Pepsi"
+            )
+        )
+
+        db.collection("purchase_info").document()
+            .set(docData)
+            .addOnSuccessListener { Log.d(TAG, "DocumentSnapshot successfully written!") }
+            .addOnFailureListener { e -> Log.w(TAG, "Error writing document", e) }
+    }
+
+    override fun onClick(v: View?) {
+        Log.d(TAG, "Clicked onClickListener()")
+        when(v?.id) {
+            R.id.get_content -> getContent()
+            R.id.update_content -> updateContent()
+        }
+    }
+
 
     companion object {
         const val TAG = "MainActivity"
     }
+
 }
